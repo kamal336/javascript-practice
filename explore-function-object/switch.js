a1d5393dad0d03fc34ca62ae20459f69fc6602a9
kamal336 <akterkamal336@gmail.com>
2021-08-03 15:52:14 +0900

    switch added

---
+++ explore-function-object/switch.js
@@ -0,0 +1,43 @@
+// // if,else if,esle condition
+
+// var color = 'blue';
+
+// if(color == 'red'){
+//     console.log('This is a red color');
+// }
+// else if(color == 'black'){
+//     console.log('This is a black color');
+// }
+// else if(color == 'purple'){
+//     console.log('This is a purple color');
+// }
+// else if(color == 'sky'){
+//   //  console.log('This is a sky color');
+// }
+// else if(color == 'white'){
+//     console.log('This is a white color');
+// }
+// else if(color == 'blue'){
+//     console.log('This is a blue  color');
+// }
+// else{
+//     console.log('Color not match.');
+// }
+
+// switch starts from here
+
+var myWife = 'naim';
+
+switch(myWife){
+    case 'koli':
+        console.log('She is my college friend.')
+        break;
+    case 'dewan':
+        console.log('She was my coligue.')
+        break;
+    case 'kamol':
+        console.log('She was my best friend.')
+        break;
+    default:
+        console.log('She is my wife.');
+}
